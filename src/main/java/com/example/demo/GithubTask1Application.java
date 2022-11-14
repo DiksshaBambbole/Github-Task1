0a8aad1a2a51794146bedcc93aceead50b1232d9
unknown <diksshan999@gmail.com>
2022-11-14 18:40:12 +0530

    First Commit

---
+++ src/main/java/com/example/demo/GithubTask1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTask1Application.class, args);
+	}
+
+}
